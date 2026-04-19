4bf6ec9e7b8161540d1a95134d7d61bf16ba7ffe
rocky <rb@dustyfeet.com>
2026-04-19 15:01:18 -0400

    Release 10.0.0

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 if __name__ == "__main__":
     setup(
-        packages=find_packages(),
+        packages=["mathics_django"],
         include_package_data=True,
+        package_data={
+            "mathics_django": [
+                "autoload/*.m",
+                "doc/*.pcl",
+                "web/media/css/*.css",
+                "web/media/doc/*.png",
+                "web/media/img/*",
+                "web/media/img/favicons/*",
+                "web/media/fonts/*",
+                "web/media/js/*.js",
+                "web/media/js/**/*.js",
+                "web/media/js/mathics-threejs-backend/**/*",
+                "web/templates/**/*.html",
+                "web/media/js/mathjax/**/*",
+            ]
+        },
     )
