d99084954650d33de87eb4e66e0ffd1bcde2d06a
rocky <rb@dustyfeet.com>
2026-04-19 15:47:04 -0400

    Describe what's up with Python screwy packaging

--- setup.py
+++ setup.py
@@ -4,13 +4,18 @@
 from setuptools import find_packages, setup
 
 if __name__ == "__main__":
+    # TODO: move setup info into pyproject.toml
     setup(
         packages=find_packages(),
+        # include_package_data has to be False,
+        # or else Python will erroneously find
+        # "autoload", "doc" and others as an
+        # importable Python package, which it is not,
+        # and complain with a warning!
         include_package_data=False,
         package_data={
             "mathics_django": [
                 "autoload/settings.m",
-                "doc/",
                 "doc/*.pcl",
                 "web/media/css/*.css",
                 "web/media/doc/*.png",
