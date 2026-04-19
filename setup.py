0ad5bb9acad3eedf5383fca92eaaad48283ad924
rocky <rb@dustyfeet.com>
2026-04-19 16:46:27 -0400

    More packaging woes for 10.0.0
    This time, for sure!

--- setup.py
+++ setup.py
@@ -5,18 +5,21 @@ from setuptools import find_packages, setup
 
 if __name__ == "__main__":
     # TODO: move setup info into pyproject.toml
+    # However by having this here, we can *debug* this stuff easier.
     setup(
         packages=find_packages(),
-        # include_package_data has to be False,
-        # or else Python will erroneously find
-        # "autoload", "doc" and others as an
-        # importable Python package, which it is not,
-        # and complain with a warning!
+        # Below, we include set include_package_data to False *and*
+        # include the python files explicitly found in find_packaages
+        # above.  Without this Python warns that it can't find
+        # "autoload", "doc" and others when building a wheel.
         include_package_data=False,
         package_data={
             "mathics_django": [
                 "autoload/settings.m",
+                "doc/*.py",
                 "doc/*.pcl",
+                "web/*.py",
+                "web/controllers/*.py",
                 "web/media/css/*.css",
                 "web/media/doc/*.png",
                 "web/media/img/*",
@@ -25,7 +28,9 @@ if __name__ == "__main__":
                 "web/media/js/*.js",
                 "web/media/js/**/*.js",
                 "web/media/js/mathics-threejs-backend/**/*",
+                "web/migrations/*.py",
                 "web/templates/**/*.html",
+                "web/templatetags/*.py",
                 "web/media/js/mathjax/**/*",
             ]
         },
